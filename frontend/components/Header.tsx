ce2a09bca27e0c6778b86e69009d8e6871c26212
Arthur Hoffelder <arthurhoffelder7@gmail.com>
2025-10-28 21:22:26 -0300

    Melhorias de código e imagens

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -2,6 +2,7 @@
 import {useState} from "react";
 import styles from "./Header.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 
 export default function Header() {
@@ -15,7 +16,7 @@ export default function Header() {
         <ul className={styles["nav-links"]}>
           <li><a href="#">Início</a></li>
           <li><a href="#">Cursos</a></li>
-          <li><a href="#">Eventos</a></li>
+          <li><Link href="/events-news">Eventos</Link></li>
           <li><a href="#">Instituição</a></li>
           <li><a href="#">Orientadores</a></li>
           <li><a href="#">Contate-nos</a></li>
