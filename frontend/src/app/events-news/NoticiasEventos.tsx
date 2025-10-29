d95d5ab3b9cb9feb8ee1a9f5bbc70d45ebf89cbb
Arthur Hoffelder <arthurhoffelder7@gmail.com>
2025-10-28 21:45:34 -0300

    Bug da importação do css para a pagina eventos corrigido

--- frontend/src/app/events-news/NoticiasEventos.tsx
+++ frontend/src/app/events-news/NoticiasEventos.tsx
@@ -1,26 +1,26 @@
 "use client";
 
 import Image from "next/image";
-import "./page.module.css";
+import styles from "./page.module.css"; 
 
 export default function NoticiasEventos() {
   return (
-    <main className="app-container">
-      <section className="noticias-container">
-        <div className="noticias-left">
-          <h1 className="titulo">Notícias e eventos</h1>
+    <main className={styles["app-container"]}>
+      <section className={styles["noticias-container"]}>
+        <div className={styles["noticias-left"]}>
+          <h1 className={styles.titulo}>Notícias e eventos</h1>
 
-          <div className="imagem-wrapper">
+          <div className={styles["imagem-wrapper"]}>
             <Image
-              src="/evento1.png"
+              src="/images/image.jpg"
               alt="Evento de tecnologia"
               width={600}
               height={400}
-              className="imagem-evento"
+              className={styles["imagem-evento"]}
             />
           </div>
 
-          <p className="descricao">
+          <p className={styles.descricao}>
             Lorem ipsum dolor sit amet. Vel adipisci commodi non vitae aliquid hic unde cupiditate. 
             33 ratione galisum ut odio quod sed voluptatem rerum id sint esse sit totam corporis qui 
             earum dolores ab omnis asperiores! Ea omnis minus et ratione veritatis a nobis optio At 
@@ -29,17 +29,18 @@ export default function NoticiasEventos() {
             sapiente repellendus.
           </p>
 
-          <button className="botao-eventos">Veja todos os eventos</button>
+          <button className={styles["botao-eventos"]}>Veja todos os eventos</button>
         </div>
-        <div className="noticias-right">
-          <div className="lista-eventos">
+
+        <div className={styles["noticias-right"]}>
+          <div className={styles["lista-eventos"]}>
             {Array.from({ length: 10 }).map((_, i) => (
-              <div key={i} className="evento-item">
+              <div key={i} className={styles["evento-item"]}>
                 <h3>
                   Nome do evento: <span>Subtítulo</span>
                 </h3>
-                <span className="data">15 de setembro, 2025</span>
-                <span className="seta">→</span>
+                <span className={styles.data}>15 de setembro, 2025</span>
+                <span className={styles.seta}>→</span>
               </div>
             ))}
           </div>
