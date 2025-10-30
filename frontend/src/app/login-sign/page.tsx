f785ddae3c87e866abc3b009689fe8c1cb0e1c99
Bianca Cabral <bibicabral5@gmail.com>
2025-10-29 22:07:15 -0300

    Introdução da responsividade mobile

--- frontend/src/app/login-sign/page.tsx
+++ frontend/src/app/login-sign/page.tsx
@@ -6,65 +6,71 @@ import styles from "./page.module.css";
 
 export default function LoginRegister() {
   const [isActive, setIsActive] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+
 
   return (
     <div className={styles.loginPage}>
 
       <div className={`${styles.container} ${isActive ? styles.active : ""}`}>
+
         {/* LOGIN */}
-        <div className={`${styles.formBox} ${styles.login}`}>
-          <form onSubmit={(e) => e.preventDefault()}>
-            <h1>Login</h1>
+        {showLogin && (
+          <div className={`${styles.formBox} ${styles.login}`}>
+            <form onSubmit={(e) => e.preventDefault()}>
+              <h1>Login</h1>
 
-            <div className={styles.inputBox}>
-              <input type="text" placeholder="Nome" required />
-              {/* <FaUser className={styles.icon} />*/}
-            </div>
+              <div className={styles.inputBox}>
+                <input type="text" placeholder="Nome" required />
+                {/* <FaUser className={styles.icon} />*/}
+              </div>
 
-            <div className={styles.inputBox}>
-              <input type="password" placeholder="Senha" required />
-              {/*<FaLock className={styles.icon} />*/}
-            </div>
+              <div className={styles.inputBox}>
+                <input type="password" placeholder="Senha" required />
+                {/*<FaLock className={styles.icon} />*/}
+              </div>
 
-            <div className={styles.forgotLink}>
-              <a href="#">Esqueceu a sua senha?</a>
-            </div>
+              <div className={styles.forgotLink}>
+                <a href="#">Esqueceu a sua senha?</a>
+              </div>
 
-            <button type="submit" className={styles.btn}>Entrar</button>
-
-          </form>
-        </div>
+              <button type="submit" className={styles.btn}>Entrar</button>
+            </form>
+          </div>
+        )}
 
         {/* REGISTER */}
-        <div className={`${styles.formBox} ${styles.register}`}>
-          <form onSubmit={(e) => e.preventDefault()}>
-            <h1>Cadastre-se</h1>
+        {!showLogin && (
+          <div className={`${styles.formBox} ${styles.register}`}>
+            <form onSubmit={(e) => e.preventDefault()}>
+              <h1>Cadastre-se</h1>
 
-            <div className={styles.inputBox}>
-              <input type="text" placeholder="Nome" required />
-              {/*<FaUser className={styles.icon} />*/}
-            </div>
+              <div className={styles.inputBox}>
+                <input type="text" placeholder="Nome" required />
+                {/*<FaUser className={styles.icon} />*/}
+              </div>
 
-            <div className={styles.inputBox}>
-              <input type="email" placeholder="Email" required />
-              {/*<FaEnvelope className={styles.icon} />*/}
-            </div>
+              <div className={styles.inputBox}>
+                <input type="email" placeholder="Email" required />
+                {/*<FaEnvelope className={styles.icon} />*/}
+              </div>
 
-            <div className={styles.inputBox}>
-              <input type="password" placeholder="Senha" required />
-              {/* <FaLock className={styles.icon} />*/}
-            </div>
+              <div className={styles.inputBox}>
+                <input type="password" placeholder="Senha" required />
+                {/* <FaLock className={styles.icon} />*/}
+              </div>
 
-            <button type="submit" className={styles.btn}>Concluir</button>
-          </form>
-        </div>
+              <button type="submit" className={styles.btn}>Concluir</button>
+            </form>
+          </div>
+        )}
 
         {/* TOGGLE AREA */}
         <div className={styles.toggleBox}>
           <div className={`${styles.togglePanel} ${styles.toggleLeft}`}>
             <h1>Boas vindas!</h1>
             <p>Ainda não tem uma conta?</p>
-            <button className={styles.btnOutiline} onClick={() => setIsActive(true)}>
+            <button className={styles.btnOutiline} onClick={() => { setIsActive(true); setShowLogin(false);}}>
               Cadastrar-se
             </button>
           </div>
@@ -72,7 +78,7 @@ export default function LoginRegister() {
           <div className={`${styles.togglePanel} ${styles.toggleRight}`}>
             <h1>Boas vindas!</h1>
             <p>Já tem conta?</p>
-            <button className={styles.btnOutiline} onClick={() => setIsActive(false)}>
+            <button className={styles.btnOutiline} onClick={() => { setIsActive(false); setShowLogin(true);}}>
               Entrar
             </button>
           </div>
