88ece5e4b3be09d5153844cefa2a4a182f123195
MauricioScheffer <133931436+MauricioScheffer@users.noreply.github.com>
2025-10-30 21:56:12 -0300

    Estilizando e Construindo Section

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -25,86 +25,121 @@ export default function Home() {
       <section className={styles.hero}>
         <div className={styles.heroContent}>
           <div className={styles.heroLeft}>
-                <h1>Bem vindo ao Nosso Site</h1>
-                <p>
-                  Conheça mais sobre Tecnologias e Educação aqui. 
-                  <br />
-                  Se inscreva em Eventos e Participe da Nossa Comunidade de Estudantes de T.I!
-                </p>
-                <button className={styles.heroButton}>Saiba mais</button>
+            <h1>Bem vindo ao Nosso Site</h1>
+            <p>
+              Conheça mais sobre Tecnologias e Educação aqui.
+              <br />
+              Se inscreva em Eventos e Participe da Nossa Comunidade de Estudantes de T.I!
+            </p>
+            <button className={styles.heroButton}>Saiba mais</button>
           </div>
           <div className={styles.heroRight}>
-              <Image
+            <Image
               src="/images/image.jpg"
               alt="Imagem Institucional"
               width={570}
               height={400}
               className={styles.heroImage}
-              priority/>
-              <button className={styles.heroButton}>Nossos cursos</button>
+              priority />
+            <button className={styles.heroButton}>Nossos cursos</button>
           </div>
         </div>
       </section>
 
-    {/* sobre  o curso*/}
-    <section className={styles.sobre}>
-      <h2>Sobre os cursos</h2>
-      <p className={styles.descricao}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
-        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      </p>
+      {/* sobre  o curso*/}
+      <section className={styles.sobre}>
+        <h2>Sobre os cursos</h2>
+        <p className={styles.descricao}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+          nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
 
-      <div className={styles.cursosContainer}>
-        {/* CARD 1 - TDS */}
-        <div className={styles.card}>
-          <div className={styles.tag}>TDS</div>
-          <Image
-            src="/images/tds.jpg"
-            alt="Curso Técnico em Desenvolvimento de Sistemas"
-            width={400}
-            height={250}
-            className={styles.cardImage}
-          />
-          <div className={styles.cardContent}>
-            <h3>Técnico em Desenvolvimento de Sistemas</h3>
-            <p>Integrado ao Ensino Médio. Lorem ipsum dolor sit amet.</p>
-            <a href="#" className={styles.verMais}>VEJA MAIS</a>
+        <div className={styles.cursosContainer}>
+          {/* CARD 1 - TDS */}
+          <div className={styles.card}>
+            <div className={styles.tag}>TDS</div>
+            <Image
+              src="/images/tds.jpg"
+              alt="Curso Técnico em Desenvolvimento de Sistemas"
+              width={400}
+              height={250}
+              className={styles.cardImage}
+            />
+            <div className={styles.cardContent}>
+              <h3>Técnico em Desenvolvimento de Sistemas</h3>
+              <p>Integrado ao Ensino Médio. Lorem ipsum dolor sit amet.</p>
+              <a href="#" className={styles.verMais}>VEJA MAIS</a>
+            </div>
+          </div>
+
+          {/* CARD 2 - TADS */}
+          <div className={styles.card}>
+            <div className={styles.tag}>TADS</div>
+            <Image
+              src="/images/tads.jpg"
+              alt="Curso de Análise e Desenvolvimento de Sistemas"
+              width={400}
+              height={250}
+              className={styles.cardImage}
+            />
+            <div className={styles.cardContent}>
+              <h3>Tecnólogo em Análise e Desenvolvimento de Sistemas</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <a href="#" className={styles.verMais}>VEJA MAIS</a>
+            </div>
           </div>
         </div>
 
-        {/* CARD 2 - TADS */}
-        <div className={styles.card}>
-          <div className={styles.tag}>TADS</div>
+        <div className={styles.buttons}>
+          <button className={styles.btn}>Veja também os projetos do TDS</button>
+          <button className={styles.btn}>Veja também os projetos do TADS</button>
+        </div>
+
+        <p className={styles.linkInstituicao}>
+          Acesse o site do IFSUL para saber mais sobre a instituição &gt;
+        </p>
+      </section>
+
+      {/* news e eventos */}
+      <section className={styles.news}>
+        <h1>Notícias e Eventos</h1>
+
+        <div className={styles.newsContent}>
+          <div className={styles.cardLeft}>
           <Image
-            src="/images/tads.jpg"
-            alt="Curso de Análise e Desenvolvimento de Sistemas"
-            width={400}
+            src="/images/image.jpg"
+            alt="Notícias e Eventos"
+            width={375}
             height={250}
             className={styles.cardImage}
           />
-          <div className={styles.cardContent}>
-            <h3>Tecnólogo em Análise e Desenvolvimento de Sistemas</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <a href="#" className={styles.verMais}>VEJA MAIS</a>
-          </div>
+
+          <p className={styles.descricao}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Deserunt dolorum sequi, dolorem possimus amet voluptatibus
+            at asperiores tempore debitis illo aspernatur blanditiis tenetur
+            labore in, cupiditate, nulla temporibus ab. Quas.</p>
         </div>
-      </div>
 
-      <div className={styles.buttons}>
-        <button className={styles.btn}>Veja também os projetos do TDS</button>
-        <button className={styles.btn}>Veja também os projetos do TADS</button>
-      </div>
+        <div className={styles.cardRight}>
+          <div className="list">
+            <ul className={styles.listNews}>
+              <li><h2>Nome do Evento: Subtítulo</h2>
+              <p>15 de setembro, 2025</p>
+              <span className={styles.seta}>→</span>
+              </li>
+            </ul>
+          </div>
+            <button className={styles.botaoEventos}>Veja todos os eventos</button>
+        </div>
+        
+        </div>
+      </section>
+      {/* professores */}
 
-      <p className={styles.linkInstituicao}>
-        Acesse o site do IFSUL para saber mais sobre a instituição &gt;
-      </p>
-    </section>
+      {/* sobre o portal */}
 
-    {/* news e eventos */}
-    {/* professores */}
-    {/* sobre o portal */}
-    
 
     </main>
   );
